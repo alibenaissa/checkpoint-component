c7941a83d0a2ed83186fa7a92bb9845b3207fd1f
ali <alibenaissa565@gmail.com>
2021-11-28 17:23:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Card, Button} from 'react-bootstrap';
+import neymar from './neymar.jpg'
+import messi from './messi.jpg'
+import mbape from './mbape.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="cardtotal">
+    <Card style={{ width: '18rem' }}>
+  <Card.Img src={messi} />
+  <Card.Body>
+    <Card.Title className="messi"> lionel messi </Card.Title>
+    <Card.Text>
+    Lionel Messi, parfois surnommé Leo Messi, né le 24 juin 1987 à Rosario en Argentine,
+     est un footballeur international argentin évoluant au poste d'attaquant au Paris Saint-Germain.
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={neymar} />
+  <Card.Body>
+    <Card.Title className="neymar"> neymar </Card.Title>
+    <Card.Text>
+    Neymar da Silva Santos Júnior, dit Neymar Jr., plus couramment appelé Neymar,
+     né le 5 février 1992 à Mogi das Cruzes (Brésil, État de São Paulo),
+      est un footballeur international brésilien qui évolue au poste d'attaquant
+       au Paris Saint-Germain. Il est également le capitaine de l'équipe nationale du Brésil depuis 2014.
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={mbape}  />
+  <Card.Body>
+    <Card.Title className="mbape">Mbappé</Card.Title>
+    <Card.Text>
+    Kylian Mbappé Lottin1 [kiljan m͡bape lɔtɛ̃]note 1, né le 20 
+    décembre 1998 dans le 19e arrondissement de Paris, est un
+     footballeur international français qui évolue au poste d'attaquant au Paris Saint-Germain, en Ligue 1.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+</div>
   );
 }
 
